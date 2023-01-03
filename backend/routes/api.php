8d56563778b25eb649b289b22b321b24df17112d
marijokovalenic <marijokovalenic@github.com>
2023-01-03 05:58:01 +0100

    init

---
+++ backend/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\BookController;
+use App\Models\Book;
+use Illuminate\Http\Request;
+use App\Http\Resources\BookResource;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/books', function() {
+    return BookResource::collection(Book::all());
+});
+
+Route::get('/book/{id}', function($id) {
+    return new BookResource(Book::findOrFail($id));
+});
+
+Route::put('/book/{id}', [BookController::class, 'update']);
+Route::delete('/book/{id}', [BookController::class, 'destroy']);
+Route::post('/book', [BookController::class, 'store']);
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
